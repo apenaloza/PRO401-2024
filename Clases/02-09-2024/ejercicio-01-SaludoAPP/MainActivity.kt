dd6e82e7b40effd31d0fa3abd71d3e5e7360e148
apenaloza <alejandro@dhcp.cl>
2024-09-04 14:32:59 -0400

    Ejercicios clases al 04/09/2024

---
+++ Clases/02-09-2024/ejercicio-01-SaludoAPP/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.saludoapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val nombre = findViewById<EditText>(R.id.etNombre)
+        val saludo = findViewById<TextView>(R.id.tvSaludo)
+        val btnSaludar = findViewById<Button>(R.id.btnSaludar)
+
+        btnSaludar.setOnClickListener{
+            val name = nombre.text.toString()
+            saludo.text = "Hola, $name"
+        }
+
+
+    }
+}
---
+++ Clases/04-09-2024/Ejercicio-03-Calculadora/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.calculadora
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val numeroUno = findViewById<EditText>(R.id.etNumUno)
+        val numeroDos = findViewById<EditText>(R.id.etNumDos)
+        val resultado = findViewById<TextView>(R.id.txtResultado)
+        val btnSumar = findViewById<Button>(R.id.btnSumar)
+        val btnRestar = findViewById<Button>(R.id.btnRestar)
+        val btnMultiplicar = findViewById<Button>(R.id.btnMultiplicar)
+        val btnDividir = findViewById<Button>(R.id.btnDividir)
+
+
+        btnSumar.setOnClickListener{
+            val num1 = numeroUno.text.toString().toInt()
+            val num2 = numeroDos.text.toString().toInt()
+            val total = num1+num2
+            resultado.text = "La suma es: $total"
+        }
+        btnRestar.setOnClickListener{
+            val num1 = numeroUno.text.toString().toInt()
+            val num2 = numeroDos.text.toString().toInt()
+            val total = num1-num2
+            resultado.text = "La resta es: $total"
+        }
+        btnMultiplicar.setOnClickListener{
+            val num1 = numeroUno.text.toString().toInt()
+            val num2 = numeroDos.text.toString().toInt()
+            val total = num1*num2
+            resultado.text = "La multiplicacion es: $total"
+        }
+        btnDividir.setOnClickListener{
+            val num1 = numeroUno.text.toString().toInt()
+            val num2 = numeroDos.text.toString().toInt()
+            val total = num1/num2
+            resultado.text = "La division es: $total"
+        }
+
+    }
+}
