a6209d81940b0fff562f0f2cf15c399005c3037e
Alejandro Peñaloza <alejandro@dhcp.cl>
2024-10-07 19:50:29 -0300

    fixed

--- Clases/30-09-2024/crud/DatabaseHelper.kt
+++ Clases/30-09-2024/crud/DatabaseHelper.kt
@@ -6,7 +6,7 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {{ 
     companion object {
         const val DATABASE_NAME = "usuarios.db"
         const val TABLE_NAME = "tabla_usuarios"
